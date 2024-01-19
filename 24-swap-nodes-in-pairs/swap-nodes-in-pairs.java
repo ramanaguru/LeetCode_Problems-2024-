3d9841cfab14ec638784dd9775c5dccfe284ac65
Ramanaguru <121872409+ramanaguru@users.noreply.github.com>
2024-01-19 07:48:29 +0530

    Time: 0 ms (100.00%) | Memory: 41.1 MB (42.39%) - LeetSync

--- 24-swap-nodes-in-pairs/swap-nodes-in-pairs.java
+++ 24-swap-nodes-in-pairs/swap-nodes-in-pairs.java
@@ -9,30 +9,36 @@
  * }
  */
 class Solution {
-    public ListNode swapPairs(ListNode A) {
-       // declare two nodes and assign at 1st and 2nd position (prev and curr)
+    public ListNode swapPairs(ListNode head) {
+        // declare two nodes and assign at 1st and 2nd position (prev and curr)
         // swap values of these 2 nodes
         // update prev = curr.next
-        // update curr = prev.next -->  i.e by 2 positions.
-        // check curr.next for null pointer expection
-
-        if(A == null) return null;
-        if(A.next == null ) return A;
-
-        ListNode prev = A;
-        ListNode curr = A.next;
-
-        while(curr !=null){
-            int tempVal = prev.val;
-            prev.val = curr.val;
-            curr.val = tempVal;
-            if(curr.next == null){
-                return A;
-            }
-            prev = curr.next;
-            curr = prev.next; 
+        // update curr = prev.next {i.e by 2 positions.} DRY RUN IT WILL MAKE SENSE
+        // check curr.next for null pointer expection and exist
+        if(head == null){
+            return null;
         }
-        return A;
+        if(head.next == null){
+            return head;
+        }
+
+        ListNode prev = head;
+        ListNode curr = head.next;
+
+        while(curr != null){
+            int temp = prev.val;
+            prev.val = curr.val;
+            curr.val = temp;
+
+            if(curr.next == null){
+                return head;
+            }
+
+            prev = curr.next;
+            curr = prev.next;
+        }
+
+        return head;
 
     }
 }
