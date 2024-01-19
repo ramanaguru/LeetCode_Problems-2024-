3a12624462eec4f52582fcb922450043d450634c
Ramanaguru <121872409+ramanaguru@users.noreply.github.com>
2024-01-19 05:51:12 +0530

    Update remove-duplicates-from-sorted-list-ii.java

--- 82-remove-duplicates-from-sorted-list-ii/remove-duplicates-from-sorted-list-ii.java
+++ 82-remove-duplicates-from-sorted-list-ii/remove-duplicates-from-sorted-list-ii.java
@@ -12,34 +12,35 @@ class Solution {
     public ListNode deleteDuplicates(ListNode head) {
         //create a dummy and have tarck of prev & head
         
-        ListNode dummy = new ListNode(-1);
-        dummy.next = head;
+        ListNode dummy = new ListNode(-1);          
+        dummy.next = head; // dummy Node looks like {-1 -> 1--> 1--> 2--> 4}
 
-        ListNode prev = dummy ;
+        ListNode prev = dummy ;  //prev = -1
 
-        while(head != null){
-
-          if(head.next != null && head.val == head.next.val){
+        while(head != null){           
+    //first case ==> head == head.next.val  so find where should prev.next  point and head(ex our input: 1--> 1--> 2--> 4)
+            if(head.next != null && head.val == head.next.val){
               
-              while(head.next != null && head.val == head.next.val){
-                  head = head.next;
-              }
+                while(head.next != null && head.val == head.next.val){
+                  head = head.next; // keep on check whether head is equls to  head.next.val
+                }
 
-              prev.next = head.next;
-          }
-          else{
-              prev = prev.next;
-          }
+                //if not update prev.next pointer ()
+                prev.next = head.next;
+            }
 
-          head = head.next;
+            //Second case ==> head.val != head.next.val (ex: 1--> 2--> 4 -> 4 -> 5)
+            //simply update prev pointer
+            else{
+                prev = prev.next;
+            }
+
+            head = head.next;
 
         }
 
         return dummy.next;
         
-
-
-
 
     }
 }
