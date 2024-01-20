a9a599eba085c3d89f0b7773b4055554e056ae96
Ramanaguru <121872409+ramanaguru@users.noreply.github.com>
2024-01-20 10:47:21 +0530

    Update palindrome-linked-list.java
    Palindrome List

--- 234-palindrome-linked-list/palindrome-linked-list.java
+++ 234-palindrome-linked-list/palindrome-linked-list.java
@@ -1,18 +1,37 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
+IDEA
+
+/*
+        step - 1 : slow and fast will at starting fo the head
+
+        step - 2 :
+
+              slow      fast
+        1 --->  2  ---> 2  ---> 1
+               Mid
+
+        Step - 3:
+
+             reverse(slow.next); ===>  that will be ( 1 ---> 2)
+
+             move the slow pointer one place ahead
+
+             head           slow
+             1 --->  2  ---> 1  ---> 2
+
+        Step - 4 : check A.val and slow.val if equals return true else false
+*/
+
+
+CODE
+
 class Solution {
     public boolean isPalindrome(ListNode head) {
+
+
         ListNode slow = head;
         ListNode fast = head;
 
+        //Find the middle
         while(fast.next != null && fast.next.next != null){
             slow = slow.next;
             fast = fast.next.next;
@@ -27,7 +46,7 @@ class Solution {
                 return false;
             }
             slow = slow.next;
-            head = head.next; 
+            head = head.next;
         }
 
         return true;
