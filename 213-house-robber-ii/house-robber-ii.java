dc71b1a15eca461e038e3d51b8cff5c52eacc545
Ramanaguru <121872409+ramanaguru@users.noreply.github.com>
2024-01-21 09:27:41 +0530

    Time: 0 ms (100.00%) | Memory: 41 MB (32.50%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.java
+++ 213-house-robber-ii/house-robber-ii.java
@@ -1,31 +1,44 @@
 class Solution {
     public int rob(int[] nums) {
         int n = nums.length;
-        
-        if (n == 1) {
-            return nums[0];
+        if(n == 1)return nums[0];
+
+        int nums1[] = new int[n];
+        int nums2[] = new int[n];
+
+        for(int i = 0; i < n; i++){
+            if(i != n-1){
+                nums1[i] = nums[i];
+            }
+            if( i != 0){
+                nums2[i] = nums[i];
+            }
         }
 
-        int[] dp1 = new int[n]; // Robbing from the 1st house
-        int[] dp2 = new int[n]; // Robbing from the 2nd house onwards
+        int ans1 = func(nums1);
+        int ans2 = func(nums2);
 
-        // Initialize the first two elements of dp1 and dp2
-        dp1[0] = nums[0];
-        dp2[0] = 0;
-        dp1[1] = Math.max(nums[0], nums[1]);
-        dp2[1] = nums[1];
+        return Math.max(ans1, ans2);
+    }
+    public int func(int nums[]){
+        int n = nums.length;
 
-        // Populate dp1 (Robbing from the 1st house)
-        for (int i = 2; i < n - 1; i++) {
-            dp1[i] = Math.max(nums[i] + dp1[i - 2], dp1[i - 1]);
+        int prev = nums[0];
+        int prev2 =0;
+
+        for(int i =1; i< n; i++){
+            int take = nums[i];
+            if(i > 1){
+                take += prev2;
+            }
+            int notTake = prev;
+
+            int curr = Math.max(take, notTake);
+
+            prev2 = prev;
+            prev = curr;
         }
 
-        // Populate dp2 (Robbing from the 2nd house onwards)
-        for (int i = 2; i < n; i++) {
-            dp2[i] = Math.max(nums[i] + dp2[i - 2], dp2[i - 1]);
-        }
-
-        // Return the maximum value between the last elements of dp1 and dp2
-        return Math.max(dp1[n - 2], dp2[n - 1]);
+        return prev;
     }
 }
