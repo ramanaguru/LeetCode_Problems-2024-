5b8a4457babdd7a89cbf92c1f69fff00a339b31e
Ramanaguru <121872409+ramanaguru@users.noreply.github.com>
2024-01-21 08:21:50 +0530

    Time: 0 ms (100.00%) | Memory: 41.1 MB (23.47%) - LeetSync

--- 198-house-robber/house-robber.java
+++ 198-house-robber/house-robber.java
@@ -1,6 +1,7 @@
 class Solution {
     public int rob(int[] nums) {
-
+        //Tabulation  will get submitted bbut we can still optimise
+        //Tc: O(N) ; Sc :O(N)
         int n = nums.length;
         int ans = Integer.MIN_VALUE;
 
@@ -10,24 +11,21 @@ class Solution {
         }
 
         int dp[] = new int[n];
-        
-        Arrays.fill(dp, -1);
+        dp[0] = nums[0];
 
-        ans = memo(nums, n-1 , dp);
-        return ans;
+        for(int idx = 1 ; idx < n; idx++){
+            int take = nums[idx];
+            if(idx > 1){
+                take += dp[idx-2];
+            }
+            int notTake = dp[idx-1];
+
+            dp[idx] = Math.max(take , notTake);
+
+        }
+
+        return dp[n-1];
 
     }
-    public int memo(int nums[], int idx, int dp[]){
-        if(idx == 0) return nums[idx];
 
-        if(idx < 0) return 0;
-
-        if(dp[idx] != -1) return dp[idx];
-
-        int take    =  nums[idx] + memo(nums, idx -2, dp);
-
-        int notTake =  memo(nums, idx-1,dp);
-
-        return dp[idx] = Math.max(take,notTake);
-    }
 }
