96ad2a1a9a388a31dc9eb867edeacfcc43df2f26
Ramanaguru <121872409+ramanaguru@users.noreply.github.com>
2024-01-22 08:17:59 +0530

    Time: 1 ms (81.28%) | Memory: 41.4 MB (45.82%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.java
+++ 14-longest-common-prefix/longest-common-prefix.java
@@ -5,7 +5,7 @@ class Solution {
         }
         StringBuilder ans = new StringBuilder();
 
-        for(int i = 0; i < strs[0].length(); ++i){
+        for(int i = 0; i < strs[0].length(); i++){
             for(String s : strs){
                 if(i == s.length() || s.charAt(i) != strs[0].charAt(i)){
                     return ans.toString();
