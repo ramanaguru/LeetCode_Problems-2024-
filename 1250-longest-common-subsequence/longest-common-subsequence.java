61a20c63488b0cca61a89d96db45d476a78f20d6
Ramanaguru <121872409+ramanaguru@users.noreply.github.com>
2024-01-25 09:32:45 +0530

    Update longest-common-subsequence.java

--- 1250-longest-common-subsequence/longest-common-subsequence.java
+++ 1250-longest-common-subsequence/longest-common-subsequence.java
@@ -1,7 +1,77 @@
+Recurison ---->  Memoisation ----> Tabulation
+
+====================================================================================================================================================
+1. Recursion (Gives TLE)
+
+    //Tc :O(2 ^(n + m))  ; Sc: O(min(n, m) + n)
+    
+    class Solution {
+    public int longestCommonSubsequence(String text1, String text2) {
+        int n  = text1.length();
+        int m = text2.length();
+
+        int ans = lcsRecursion(n-1, m -1, text1, text2);
+        
+        return ans;
+    }
+    public int lcsRecursion(int i, int j, String t1, String t2){
+        if(i < 0 || j < 0) return 0;
+
+        if(t1.charAt(i) == t2.charAt(j)){
+            return 1 + lcsRecursion(i-1, j -1, t1, t2);
+        }
+
+        return Math.max(lcsRecursion(i-1,j, t1,t2) , lcsRecursion(i,j-1, t1,t2));
+
+    }
+}
+
+
+=======================================================================================================================================================
+2. memoisation (Accepted)
+
+    //Tc : O(n * m)  ; Sc : O(n * m + n)
+
 class Solution {
     public int longestCommonSubsequence(String text1, String text2) {
+        
+        int n  = text1.length();
+        int m = text2.length();
 
-        //Tc : O(n * m)  ; Sc : O(n * m)
+        int dp[][] = new int[n][m];
+        for(int row[] : dp){
+            Arrays.fill(row, -1);
+        }
+
+        int ans = lcsMemo(n-1, m -1, text1, text2, dp);
+        
+        return ans;
+    }
+
+    public int lcsMemo(int i, int j, String t1, String t2, int dp[][]){
+        if(i < 0 || j < 0) return 0;
+
+        if(dp[i][j] != -1){
+            return dp[i][j];
+        }
+
+        if(t1.charAt(i) == t2.charAt(j)){
+            return dp[i][j] = 1 + lcsMemo(i-1, j -1, t1, t2,dp);
+        }
+
+        return dp[i][j] =Math.max(lcsMemo(i-1,j, t1,t2,dp) , lcsMemo(i,j-1, t1,t2,dp));
+
+    }
+}
+
+=======================================================================================================================================================
+3. Tabulation (Accepted)
+
+    //Tc : O(n * m)  ; Sc : O(n * m)
+    
+    class Solution {
+    public int longestCommonSubsequence(String text1, String text2) {
+
         int n  = text1.length();
         int m = text2.length();
 
