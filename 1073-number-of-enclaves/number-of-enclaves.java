303b4a44a0afaa6ce6b780d0d3ab6d673cbed233
Ramanaguru <121872409+ramanaguru@users.noreply.github.com>
2024-02-01 11:12:36 +0530

    Time: 8 ms (78.62%) | Memory: 56.2 MB (58.78%) - LeetSync

--- 1073-number-of-enclaves/number-of-enclaves.java
+++ 1073-number-of-enclaves/number-of-enclaves.java
@@ -1,10 +1,14 @@
 class Solution {
     public int numEnclaves(int[][] grid) {
+
+        //TC : O(N * M )  ;   SC : O(N * M)
         int n = grid.length;
         int m = grid[0].length;
 
         boolean vis[][] = new boolean[n][m];
 
+
+        // check for boundary , qn states exit from boundary 
         for(int i = 0; i < n; i++){
             for(int j = 0; j < m; j++){
                 if(i == 0|| j == 0 || i == n-1 || j == m-1){
