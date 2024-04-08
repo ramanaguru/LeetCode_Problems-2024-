ec4461c26067c884d06f4704322154af0e8aa5d8
Ramanaguru <121872409+ramanaguru@users.noreply.github.com>
2024-04-08 09:33:36 +0530

    Time: 0 ms (100.00%) | Memory: 89.7 MB (57.33%) - LeetSync

--- 2047-find-a-peak-element-ii/find-a-peak-element-ii.java
+++ 2047-find-a-peak-element-ii/find-a-peak-element-ii.java
@@ -1,5 +1,6 @@
 class Solution {
     public int[] findPeakGrid(int[][] mat) {
+        //TC : O(n log(m))     ;   SC : O(1)
         int n = mat.length;
         int m = mat[0].length;
 
@@ -7,25 +8,31 @@ class Solution {
         int high = m-1;
 
         while(low <= high){
-            
             int mid = (low + high)/2;
+            // we find maximum because we have to check top, bottom, left, right if we use maximum then our element is definitely
+            // greater than top and bottom. so we need to check only left and right i.e. our problem is reduced to find peak in 1d
 
             int row = findmaxi(mat, mid);
+
             int left = -1;
             int right = -1;
+
+            // handling edge cases
             if(mid - 1 >= 0){
                 left = mat[row][mid -1];
             }
             if(mid + 1 < m){
                 right = mat[row][mid + 1];
             }
-
+            // we find peak element
             if(mat[row][mid] > left && mat[row][mid] > right){
                 return new int[]{row,mid};
             }
+            // our peak is on the right side of mid so eliminate the left part  
             else if(mat[row][mid] > left){
                 low = mid + 1;
             }
+            // our peak is on the left side of mid so eliminate the right part
             else{
                 high = mid - 1;
             }
@@ -37,6 +44,7 @@ class Solution {
     }
 
     public int findmaxi(int mat[][], int mid){
+         // find row index of max element and col index is given by mid
         int index = -1;
         int maxi = Integer.MIN_VALUE;
 
